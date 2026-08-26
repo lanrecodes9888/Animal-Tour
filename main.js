0bbf426b9f0fa71990c8db5e643c32d9efce4c97
lanrecodes9888 <llare9888@gmail.com>
2026-08-26 21:17:48 +0100

    update code

--- main.js
+++ main.js
@@ -159,13 +159,13 @@ function generateHtml (info) {
          ${info[index].about}
         </div>
 
-      <button class="left js-left">
+      <button class="left js-right">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
               <path fill-rule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
             </svg>
         </button>
 
-        <button class="right js-right">
+        <button class="right js-left">
            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
               <path fill-rule="evenodd" d="M7.28 7.72a.75.75 0 0 1 0 1.06l-2.47 2.47H21a.75.75 0 0 1 0 1.5H4.81l2.47 2.47a.75.75 0 1 1-1.06 1.06l-3.75-3.75a.75.75 0 0 1 0-1.06l3.75-3.75a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
             </svg> 
